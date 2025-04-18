446b549c52cbd89e78eee5bf256dac74076b2d80
Tomar1907 <soumyatomar1908@gmail.com>
2025-04-18 10:53:58 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let imgBox = document.getElementById("imgBox")
-let qrImage = document.getElementById("qrImage")
-let qrText = document.getElementById("qrText")
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
 
 function generateQR(){
     if(qrText.value.length > 0){
